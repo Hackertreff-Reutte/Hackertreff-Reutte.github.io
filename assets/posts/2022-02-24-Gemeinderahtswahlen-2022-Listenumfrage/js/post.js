4ed93cd94e56a8b3897391cbf16b4aa84a5f6dd1
noahreinalter <38698485+noahreinalter@users.noreply.github.com>
2022-02-25 00:16:30 +0100

    remove commented code

--- assets/posts/2022-02-24-Gemeinderahtswahlen-2022-Listenumfrage/js/post.js
+++ assets/posts/2022-02-24-Gemeinderahtswahlen-2022-Listenumfrage/js/post.js
@@ -337,35 +337,3 @@ Papa.parse("/assets/posts/2022-02-24-Gemeinderahtswahlen-2022-Listenumfrage/csv/
     createAllOptionalInformation(results);
 	}
 });
-/*
-
-const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-];
-
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: [0, 10, 5, 2, 20, 30, 45],
-    }]
-};
-
-const config = {
-    type: 'line',
-    data: data,
-    options: {}
-};
-
-const myChart = new Chart(
-    document.getElementById('chart'),
-    config
-  );
-  */
